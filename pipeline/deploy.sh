d7b099f524254a70b976c18f0f0e668ac74c2a2f
Johannes Vogel <43568009+begvog@users.noreply.github.com>
2023-06-27 13:00:36 +0000

    add initilial deploy step

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -5,3 +5,17 @@ set -e
 echo "$0"
 
 echo "nothing to do so far"
+
+
+# git clone webserver_repo
+rm -rf projectx_webserver/
+git clone https://$MACHINE_USER_TOKEN@github.com/begvog/projectx_webserver.git
+
+# cp all files from our code/ folder into webserver repo /<root> folder
+cd projectx_webserver/
+rm -rf *
+cp -r ../code/* .
+
+git add --all
+git commit -m "$(date)" || true
+git push --all
