83fe022510e94eb0a5db7b86e77443d83017e856
AnushaPatelHS <135839228+AnushaPatelHS@users.noreply.github.com>
2023-06-29 07:41:57 +0000

    Fix-1

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -5,3 +5,19 @@ set -e
 echo "$0"
 
 echo "nothing to do so far"
+
+
+# git clone webserver_repo
+rm -rf projectx_webserver/
+git clone https://$MACHINE_USER_TOKEN@github.com/AnushaPatelHS/projectx_webserver.git
+
+#cp all files from our code/ folder into webserver repo /<root> folder
+#cp all the 
+cd projectx_webserver/
+rm -rf *
+cp -r ../code/* .
+
+git add --all
+git commit -m "$(date) || true"
+git push --all
+rm -rf projectx_webserver/
