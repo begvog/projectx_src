33ff7f8395944db3650b872dc4f3b9907661d268
AnushaPatelHS <135839228+AnushaPatelHS@users.noreply.github.com>
2023-06-29 08:08:38 +0000

    added user name

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -11,9 +11,12 @@ echo "nothing to do so far"
 rm -rf projectx_webserver/
 git clone https://$MACHINE_USER_TOKEN@github.com/AnushaPatelHS/projectx_webserver.git
 
+
 #cp all files from our code/ folder into webserver repo /<root> folder
 #cp all the 
 cd projectx_webserver/
+git config user.email "user@machine.com"
+git config user.name "machine_user"
 rm -rf *
 cp -r ../code/* .
 
